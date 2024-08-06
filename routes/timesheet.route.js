da9be1496353cf506230a9d45691cfb4cf62b797
Rochiyat <rochiyat@bithealth.co.id>
2024-08-06 20:25:00 +0700

    add endpoint last-week

--- routes/timesheet.route.js
+++ routes/timesheet.route.js
@@ -2,9 +2,9 @@ const { Router } = require('express');
 const { SuccessResponseObject } = require('../common/http');
 const timesheetController = require('../controllers/timesheet.controller');
 
-const router = Router();
+const r = Router();
 
-router.get('/', (req, res) => res.json(new SuccessResponseObject('demo path live 🚀')));
-router.get('/last-week', timesheetController.last);
+r.get('/', (req, res) => res.json(new SuccessResponseObject('demo path live 🚀')));
+r.get('/last-week', timesheetController.last);
 
-module.exports = router;
+module.exports = r;
